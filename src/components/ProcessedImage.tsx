a66f76f0a8900ccfc463b23ef416707190f6dc10
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2023-02-03 12:32:52 +0000

    jimp build done

--- src/components/ProcessedImage.tsx
+++ src/components/ProcessedImage.tsx
@@ -4,39 +4,40 @@ import Jimp from "jimp";
 import { useEffect, useState } from "react";
 
 interface PhotoProps {
-  photo: Photo;
+  photo: Photo,
+  percentage: number
 }
 
 
-const ProcessedImage: React.FC<PhotoProps> = ({ photo }) => {
+const ProcessedImage: React.FC<PhotoProps> = ({ photo, percentage }) => {
   // const [imgData, setData] = useState<string | undefined>();
 //   let outData: string | undefined = undefined;
   
     const [outData, setOutData] = useState<string|undefined>(undefined);
 
   useEffect(() => {
-    console.log("Process");
+    // console.log("Process");
     const instagram: number = 1080;
-    const border: number = 7;
+    const border: number = percentage;
 
     const processImage = async (photo: Photo) => {
-        console.log("called")
+        // console.log("called")
 
       let inImagePath: string = photo.webPath!;
-      console.log(inImagePath);
+    //   console.log(inImagePath);
       let outImage = new Jimp(instagram, instagram, "#FFFFFF");
 
       Jimp.read(inImagePath)
         .then((image) => {
-            console.log(image.bitmap.width);
-            console.log(Math.floor((instagram * (100-border)) / 100))
+            // console.log(image.bitmap.width);
+            // console.log(Math.floor((instagram * (100-border)) / 100))
           image.scaleToFit(
             Math.floor((instagram * (100-border)) / 100),
             Math.floor((instagram * (100-border)) / 100),
             Jimp.RESIZE_BILINEAR,
             (err, croppedImg) => {
               if (err) throw err;
-              console.log(croppedImg.bitmap.width)
+            //   console.log(croppedImg.bitmap.width)
 
               let x = Math.floor((instagram - croppedImg.getWidth()) / 2);
               let y = Math.floor((instagram - croppedImg.getHeight()) / 2);
@@ -58,7 +59,11 @@ const ProcessedImage: React.FC<PhotoProps> = ({ photo }) => {
       
     };
     processImage(photo).catch(console.error);
-  }, []);
+
+    return () => {
+        setOutData(undefined);
+    }
+  }, [photo, percentage]);
 
   if (outData === undefined){
       return (
--- src/pages/EditPhoto.tsx
+++ src/pages/EditPhoto.tsx
@@ -15,8 +15,10 @@ import {
   IonButton,
   IonSkeletonText,
   IonThumbnail,
+  IonRange,
+  IonLabel,
 } from "@ionic/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { usePhotoGalleryFromCamera } from "../hooks/useGallery";
 import "./EditPhoto.css";
 import {
@@ -25,11 +27,15 @@ import {
   chatbubbleOutline,
   sendOutline,
   bookmarkOutline,
+  addOutline,
+  removeOutline
+  
 } from "ionicons/icons";
 import ProcessedImage from "../components/ProcessedImage";
 
 const EditPhoto: React.FC = () => {
   const { takePhoto, selectedPhoto } = usePhotoGalleryFromCamera();
+  const [ borderPercentage, setBorderPercentage] = useState<number>(7);
 
   useEffect(() => {
     takePhoto();
@@ -38,7 +44,7 @@ const EditPhoto: React.FC = () => {
 
   return (
     <IonPage>
-      <IonContent>
+      <IonContent fullscreen={true}>
         <IonCard>
           <IonRow class="ion-align-items-center ion-justify-content-center">
             <IonCol size="auto" class="ion-padding-start ion-padding-end">
@@ -46,7 +52,7 @@ const EditPhoto: React.FC = () => {
                 <IonImg src="https://ionicframework.com/docs/img/demos/avatar.svg" />
               </IonAvatar>
             </IonCol>
-            <IonCol class="profile">
+            <IonCol class="text-left">
               <IonText color="dark">
                 <strong>infinus</strong>
               </IonText>
@@ -61,7 +67,7 @@ const EditPhoto: React.FC = () => {
                          animated={true}
                          style={{ aspectRatio: "1/1" }}
                        ></IonSkeletonText> :
-            <ProcessedImage photo={selectedPhoto} />}
+            <ProcessedImage photo={selectedPhoto} percentage={borderPercentage}/>}
           </div>
 
           <IonRow>
@@ -87,6 +93,36 @@ const EditPhoto: React.FC = () => {
             </IonCol>
           </IonRow>
         </IonCard>
+        <IonCard>
+          <IonRow class="ion-align-items-center ion-justify-content-center">
+            <IonCol class="text-left ion-padding" >
+            <IonText >Border Width: {borderPercentage}%</IonText>
+            </IonCol>
+            <IonCol size="auto">
+              <IonButton size="small" fill="clear" color="dark" onClick={() => { let current = borderPercentage;
+              setBorderPercentage(current+1);}}>
+                <IonIcon icon={addOutline}></IonIcon>
+              </IonButton>
+            </IonCol>
+            <IonCol size="auto">
+              <IonButton size="small" fill="clear" color="dark" onClick={() => { let current = borderPercentage;
+              setBorderPercentage(current-1);}}>
+                <IonIcon icon={removeOutline}></IonIcon>
+              </IonButton>
+            </IonCol >
+            </IonRow>
+            <IonRow class="ion-padding-horizontal">
+              <IonCol class="ion-padding=horizontal">
+                <IonRange class="ion-no-padding" debounce = {80} onIonChange={(e)=>{setBorderPercentage(e.target.value as number)}}></IonRange>
+        </IonCol>
+        </IonRow>
+        </IonCard>
+        
+            
+          
+         
+        
+              
       </IonContent>
     </IonPage>
   );
