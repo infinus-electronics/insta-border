40a182d52ad3f03de3d22c351c760b0de263b4d2
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2023-02-04 09:28:15 +0000

    seems to have fixed the image loading thing

--- src/components/ProcessedImageCanvas.tsx
+++ src/components/ProcessedImageCanvas.tsx
@@ -12,7 +12,7 @@ interface PhotoProps {
 
 const ProcessedImageCanvas : React.FC<PhotoProps> = ({ photo, percentage }) => {
 
-    // const [finishedCanvas, setFinishedCanvas] = useState<string>()
+    const [loaded, setLoaded] = useState<boolean>()
     const canvasRef = useRef<HTMLCanvasElement>(null);
     
 
@@ -35,10 +35,9 @@ const ProcessedImageCanvas : React.FC<PhotoProps> = ({ photo, percentage }) => {
 
     let img = new Image();
     if(photo !== undefined){
-      img.src = photo.dataUrl!;
-      
+      img.src = photo.dataUrl!;      
     }    
-    // console.log(img)
+    img.onload = () => {
 
     let targetHeight = 0;
     let targetWidth = 0;
@@ -51,33 +50,17 @@ const ProcessedImageCanvas : React.FC<PhotoProps> = ({ photo, percentage }) => {
         targetHeight = Math.floor(img.height/img.width*targetWidth);
     }
 
-    // let cur = {
-    //     width: Math.floor(img.width * 0.5),
-    //     height: Math.floor(img.height * 0.5)
-    // }
-
-    // oc.width = cur.width;
-    // oc.height = cur.height;
-
-    // octx.drawImage(img, 0, 0, cur.width, cur.height);
-
-    // while (cur.width * 0.5 > targetWidth) {
-    //     cur = {
-    //       width: Math.floor(cur.width * 0.5),
-    //       height: Math.floor(cur.height * 0.5)
-    //     };
-    //     octx.drawImage(oc, 0, 0, cur.width * 2, cur.height * 2, 0, 0, cur.width, cur.height);
-    //   }
-
+      setLoaded(true)
       let dx = Math.floor((instagram-targetWidth)/2);
       let dy = Math.floor((instagram-targetHeight)/2);
       ctx.drawImage(img, 0, 0, img.width, img.height, dx, dy, targetWidth, targetHeight);
+  }
 
       return () => {
-        
+        setLoaded(false)
       }
 
-    //   setFinishedCanvas(canvas.toDataURL("image/jpeg")!);
+
     },[photo, percentage]);
 
     return (<canvas ref={canvasRef}/>);
